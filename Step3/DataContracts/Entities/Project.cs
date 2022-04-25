fac2e6fc276f894b1d085330f14f21eeb66ec9d3
mdpmRU <udanuda@gmail.com>
2022-04-25 09:32:12 +0300

    Реализация шага 3.1

---
+++ Step3/DataContracts/Entities/Project.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataContracts.Entities
+{
+    public class Project : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public DateTime ExpirationDate { get; set; }
+
+        public DateTime MaxHours { get; set; }
+
+        public int LeaderUserId { get; set; }
+    }
+}
