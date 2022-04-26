62c935a500df6145a4c08e36209251fd4b7f66b7
mdpmRU <udanuda@gmail.com>
2022-04-26 11:30:58 +0300

    Правки
    Убраны комментарии

--- Step3/Application/Application/Program.cs
+++ Step3/Application/Application/Program.cs
@@ -1,4 +1,1 @@
-﻿//Код для тестирование зависимости
-//using DataContracts.Entities;
-//DateTime a = DateTime.Now;
-//Project apProject = new Project { Name = "raz", ExpirationDate = a, MaxHours = new DateTime(1), LeaderUserId = 12, Id = 1 };
+﻿
