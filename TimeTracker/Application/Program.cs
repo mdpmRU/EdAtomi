a3b00638fd15c6fedd44781e3e30310d629c73c1
mdpmRU <udanuda@gmail.com>
2022-05-25 21:39:46 +0300

    Update Program.cs

--- TimeTracker/Application/Program.cs
+++ TimeTracker/Application/Program.cs
@@ -11,15 +11,16 @@ TimeTrackEntryRepository timeTrackEntryRepository = new();
 
 void OnSubmitteddTimeChanged(UserData userData)
 {
-    Console.WriteLine($"Общая сумма часов: {userData.SubmittedTime}");
+    Console.WriteLine($"Общая сумма часов: {userData.SubmittedTime.Select(h => h.Value).Sum()}");
 }
 
 using var mediator = new Mediator();
 var userServices = new UserServices(userRepository, timeTrackEntryRepository, mediator);
-
+//mediator.SubscribeToSubmittedTimeChanged(OnSubmitteddTimeChanged);
 var firstUser = userServices.GetUserById(1);
-firstUser.OnSubmittedTimeChanged();
+firstUser.OnSubmittedTimeChanged(firstUser);
 firstUser.SubmitTime(2,4,"RAz");
+firstUser.SubmitTime(2, 4, "RAz");
 
 
 
